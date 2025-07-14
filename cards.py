6cf07e5f9cdb6cfa39da1a6eefb730997c480f28
abstractlyZach <zach3lee@gmail.com>
2025-07-14 12:31:16 -0700

    Give cards non-magic numbered IDs

--- cards.py
+++ cards.py
@@ -1,3 +1,6 @@
+import enum
+
+
 class Card:
     _id: int
     _name: str
@@ -15,61 +18,100 @@ class Card:
         return NotImplemented
 
 
+class CardIds(enum.Enum):
+    ACTION = 0
+    STUNT = 1
+    MOVE = 2
+    REACT = 3
+    DOUBLE_MOVE = 4
+    LOOT = 5
+    SPRINT = 6
+    SUPER_REACT = 7
+    PROFICIENT_DRAW = 8
+    BOON = 9
+
+
 class Action(Card):
-    _id = 0
+    _id = CardIds.ACTION
     _name = ""
     _description = "Gain 1 Action."
 
 
 class Stunt(Card):
-    _id = 1
+    _id = CardIds.STUNT
     _name = ""
     _description = "Gain 1 Stunt."
 
 
 class Move(Card):
-    _id = 2
+    _id = CardIds.MOVE
     _name = ""
     _description = "Move # of spaces up to your speed."
 
 
 class React(Card):
-    _id = 3
+    _id = CardIds.REACT
     _name = ""
     _description = "Gain 1 Reaction."
 
 
 class DoubleMove(Card):
-    _id = 4
+    _id = CardIds.DOUBLE_MOVE
     _name = ""
     _description = "Move # of spaces up to 2x your speed."
 
 
 class Loot(Card):
-    _id = 5
+    _id = CardIds.LOOT
     _name = ""
     _description = "Discard this card, draw 2 cards"
 
 
 class Sprint(Card):
-    _id = 6
+    _id = CardIds.SPRINT
     _name = ""
     _description = "Move # of spaces up to your speed, draw 1 card."
 
 
 class SuperReact(Card):
-    _id = 7
+    _id = CardIds.REACT
     _name = ""
     _description = "Gain 1 Reaction, draw 2 cards."
 
 
 class ProficientDraw(Card):
-    _id = 8
+    _id = CardIds.PROFICIENT_DRAW
     _name = ""
     _description = "Draw a # of cards = proficiency bonus."
 
 
 class Boon(Card):
-    _id = 9
+    _id = CardIds.BOON
     _name = ""
     _description = "Boon."
+
+
+def get_card_from_id(_id: int) -> Card:
+    match _id:
+        case CardIds.ACTION:
+            return Action()
+        case CardIds.STUNT:
+            return Stunt()
+        case CardIds.MOVE:
+            return Move()
+        case CardIds.REACT:
+            return React()
+        case CardIds.DOUBLE_MOVE:
+            return DoubleMove()
+        case CardIds.LOOT:
+            return Loot()
+        case CardIds.SPRINT:
+            return Sprint()
+        case CardIds.SUPER_REACT:
+            return SuperReact()
+        case CardIds.PROFICIENT_DRAW:
+            return ProficientDraw()
+        case CardIds.BOON:
+            return Boon()
+        case _:
+            raise Exception(f"Could not find card for id {_id}")
--- test_game.py
+++ test_game.py
@@ -1,28 +1,43 @@
 import games
 import cards
 
+
 def test_pile_print():
-    mydeck = games.Pile([cards.Action(), cards.Action()])
-    assert str(mydeck) == \
-"""
+    deck = games.get_pile_from_ids([
+        cards.CardIds.ACTION,
+        cards.CardIds.ACTION,
+    ])
+    assert str(deck) == \
+           """
 0: Gain 1 Action.
 1: Gain 1 Action.
-""".strip()
+           """.strip()
+
 
 def test_basic_draw():
-    deck = games.Pile([cards.Action(), cards.Action()])
+    deck = games.get_pile_from_ids([
+        cards.CardIds.ACTION,
+        cards.CardIds.ACTION,
+    ])
     game = games.GameState(deck=deck)
     game.draw_cards(1)
     # todo: actually check the hand by adding hand checking
     assert game.get_hand_str() == "0: Gain 1 Action."
 
+
 def test_trigger_shuffle_with_draw():
-    deck = games.Pile([cards.Action(), cards.Action()])
-    discard = games.Pile([cards.Boon()])
+    deck = games.get_pile_from_ids([
+        cards.CardIds.ACTION,
+        cards.CardIds.ACTION,
+    ])
+    discard = games.get_pile_from_ids([
+        cards.CardIds.BOON
+    ])
     game = games.GameState(deck=deck, discard=discard)
     game.draw_cards(3)
     # todo: actually check the hand by adding hand checking
     assert game.get_hand_str() == "0: Gain 1 Action.\n1: Gain 1 Action.\n2: Boon."
+
 
 def test_drawing_when_deck_and_discard_are_empty():
     deck = games.Pile([])

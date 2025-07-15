ff34513a977ffc8c97209f4ea9481da377adbb6c
abstractlyZach <zach3lee@gmail.com>
2025-07-15 11:31:22 -0700

    Add more cards to the print pile test case

--- test_game.py
+++ test_game.py
@@ -7,14 +7,20 @@ def test_pile_print():
     deck = piles.get_pile_from_ids(
         [
             CardIds.ACTION,
-            CardIds.ACTION,
+            CardIds.STUNT,
+            CardIds.MOVE,
+            CardIds.STUNT,
+            CardIds.DOUBLE_MOVE,
         ]
     )
     assert (
         str(deck)
         == """
 0: Gain 1 Action.
-1: Gain 1 Action.
+1: Gain 1 Stunt.
+2: Move # of spaces up to your speed.
+3: Gain 1 Stunt.
+4: Move # of spaces up to 2x your speed.
            """.strip()
     )
 
